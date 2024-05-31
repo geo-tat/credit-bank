aa117f992174e338feccbd07cf3a268c026162bc
Georgii <nosleep2night@yandex.ru>
2024-05-31 18:00:20 +0400

    MVP2: deal service

---
+++ deal/src/test/java/ru/neoflex/credit_bank/deal/DealApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.neoflex.credit_bank.deal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

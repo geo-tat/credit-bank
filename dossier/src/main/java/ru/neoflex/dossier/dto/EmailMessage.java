c38bc9dfb972028c248f55ccdcc75174dbeef6b7
geo-tat <nosleep2night@yandex.ru>
2024-08-16 13:18:18 +0400

    refactoring

--- dossier/src/main/java/ru/neoflex/dossier/dto/EmailMessage.java
+++ dossier/src/main/java/ru/neoflex/dossier/dto/EmailMessage.java
@@ -1,6 +1,5 @@
 package ru.neoflex.dossier.dto;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
--- statement/src/main/java/ru/neoflex/statement/config/OpenApiConfig.java
+++ statement/src/main/java/ru/neoflex/statement/config/OpenApiConfig.java
@@ -17,5 +17,4 @@ import org.springframework.context.annotation.Configuration;
         )
 )
 public class OpenApiConfig {
-
 }
